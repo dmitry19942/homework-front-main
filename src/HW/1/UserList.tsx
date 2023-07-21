ae5bea14b2723c222fd6f16caabd90fc67f56a3d
dmitry19942 <gorustovich2013@gmail.com>
2023-07-22 01:33:49 +0300

    hw2 done

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -1,6 +1,6 @@
 type AddressType = {
-  street: string; // ПОДПРАВЛЯЕМ any
-  city: string; // ПОДПРАВЛЯЕМ any
+  street: string;
+  city: string;
 };
 
 type UserType = {
@@ -8,11 +8,10 @@ type UserType = {
   name: string
   age: number
   address: AddressType
-  // ПРИДЕТСЯ САМОМУ)
 };
 
 type UserListPropsType = {
-  users: UserType[]; // ПО МОЕМУ ЧЕГО-ТО НЕ ХВАТАЕТ...
+  users: UserType[];
 };
 
 export const UserList = (props: UserListPropsType) => {
@@ -21,7 +20,7 @@ export const UserList = (props: UserListPropsType) => {
       <h2>User List:</h2>
 
       <ul>
-        {props.users.map((user) => ( // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
+        {props.users.map((user) => (
           <li key={user.id} id={`hw01-user-${user.id}`}>
             <strong>{user.name}</strong> (Age: {user.age})<strong> Address: </strong>
             {user.address.street}, {user.address.city}
